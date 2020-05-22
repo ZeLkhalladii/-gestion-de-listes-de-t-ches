a829f01aeb5161028a474aa9db6f58951cb8750e
zineb elkhallai <zinebelkhalladi.2019@gmail.com>
2020-05-22 00:39:57 +0000

    first commit

---
+++ php/user.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+</head>
+<body>
+<form action="action.php" method="post">
+        <p>firstname : <input type="text" name="firstname" /></p><br>
+        <p>lastname : <input type="text" name="lastname" /></p><br>
+        <p>Username : <input type="text" name="name" /></p><br>
+        <p>email : <input type="email" name="email" /></p><br>
+        <p>password : <input type="password" name="password" /></p><br>
+        <p>photo : <input type="photo" name="photo" /></p><br>
+        <p><input type="submit" value="create"></p>
+       </form>
+       
+</body>
+</html>
